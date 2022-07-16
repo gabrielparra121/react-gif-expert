96602dbbdbf2bbce6d6b3ab33215538842721fe6
gabrielparra121 <gabrielparra121@gmail.com>
2022-07-16 11:42:30 -0500

    primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+// import { GifGrid } from "./components/GifGrid";
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return;
+        setCategories( (c) => [newCategory, ...c]);
+    }
+
+  return (
+    <>
+        <h1>GifExpertApp</h1>
+        <AddCategory 
+            onNewCategory = { onAddCategory }
+        />
+        { 
+            categories.map( (category) => (
+                <GifGrid key={category} category={category}/>
+            ))
+        }
+    </>
+  )
+}
---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,35 @@
+// import { useEffect, useState } from "react"
+// import { getGifs } from "../helpers/getGifs"
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifItem } from "./GifItem";
+
+export const GifGrid = ({ category }) => {
+
+  const { images, isLoading } = useFetchGifs(category);
+
+  // const [images, setImages] = useState([]);
+  // const getImages  = async() => {
+  //   const newImages = await getGifs(category);
+  //   setImages(newImages)
+  // }
+  // useEffect(() => {
+  //   getImages();
+  // }, [])
+
+  return (
+    <>
+        <h3>{category}</h3>
+        {
+          isLoading && <h2>Cargando...</h2>
+        }
+        <div className="card-grid">
+          {
+            images.map((image) => (
+              // <GifItem key={image.id} title={image.title} url={image.url}/>
+              <GifItem key={image.id} {...image}/>
+            ))
+          }
+        </div>
+    </>
+  )
+}
